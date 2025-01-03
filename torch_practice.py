7f76da1d6836284af565b251b4b7c2af4420f87d
chengyi <0524eric@gmail.com>
2025-01-03 10:57:59 +0800

    module 2 load data and visualize data finished

--- torch_practice.py
+++ torch_practice.py
@@ -55,6 +55,67 @@ for y,file in zip(Y, all_files[0:4]):
     plt.title("y="+str(y.item()))
     plt.show()
 
+# create dataset
+
+class Dataset(Dataset):
+    def __init__(self,transform=None,train=True):
+        directory="./data"
+        positive="Positive"
+        negative="Negative"
+
+        positive_file_path=os.path.join(directory,positive)
+        negative_file_path=os.path.join(directory,negative)
+        positive_files=[os.path.join(positive_file_path,file) for file in  os.listdir(positive_file_path) if file.endswith(".jpg")]
+        positive_files.sort()
+        negative_files=[os.path.join(negative_file_path,file) for file in  os.listdir(negative_file_path) if file.endswith(".jpg")]
+        negative_files.sort()
+
+        self.all_files=[None]*number_of_samples
+        self.all_files[::2]=positive_files
+        self.all_files[1::2]=negative_files 
+
+        self.transform = transform
+        self.Y=torch.zeros([number_of_samples]).type(torch.LongTensor)
+        self.Y[::2] = 1
+        self.Y[1::2] = 0
+
+        if train:
+            self.all_files = self.all_files[0:30000]
+            self.Y = self.Y[0:30000]
+            self.len = len(self.all_files)
+        else:
+            self.all_files = self.all_files[30000:]
+            self.Y = self.Y[30000:]
+            self.len = len(self.all_files)
+
+    def __len__(self):
+        return self.len
+    
+    def __getitem__(self,idx):
+        image = Image.open(self.all_files[idx])
+        y = self.Y[idx]
+
+        if self.transform:
+            image = self.transform(image)
+        return image,y
+    
+
+train_dataset = Dataset(False,True)
+samples = [10, 100]
+for sample  in samples:
+    plt.imshow(train_dataset[sample][0])
+    plt.xlabel("y="+str(train_dataset[sample][1].item()))
+    plt.title("training data, sample {}".format(int(sample)))
+    plt.show()
+
+val_dataset = Dataset(False,False)
+samples = [16,103]
+for sample  in samples:
+    plt.imshow(val_dataset[sample][0])
+    plt.xlabel("y="+str(val_dataset[sample][1].item()))
+    plt.title("validation data, sample {}".format(int(sample)))
+    plt.show()
+
 
 
 
